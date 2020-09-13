00119fe91dbfd95d8fe0821efd572d7201902c13
Daniil <sitnikovdan13@gmail.com>
2020-09-13 15:15:59 +0300

    init: arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (typeof array === 'undefined' || array.length === 0) {
+        return 0;
+    }
+    return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof array === 'undefined' || array.length === 0) {
+        return ''.length;
+    }
+    return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let res = 0;
+    if (typeof array === 'undefined' || array.length === 0) {
+        console.log(0);
+        return 0;
+    } else {
+        array.forEach(el => res += el);
+        return res / array.length;
+    }
 }
